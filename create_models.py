9be12b2e1632ff3e240934c028b3ef3c87a0500e
JuliaKholodetska <55590609+JuliaKholodetska@users.noreply.github.com>
2020-12-16 14:32:29 +0300

    Update create_models.py

--- create_models.py
+++ create_models.py
@@ -1,24 +1,51 @@
-from models_to_migrate import *
+from migrate import db
+from werkzeug.security import generate_password_hash
+
+Base = db.Model
+
+class User(Base):
+    __tablename__ = 'User'
+
+    UserID = db.Column(db.INTEGER, autoincrement=True)
+    Username = db.Column(db.VARCHAR(45), primary_key=True, nullable=False)
+    FirstName = db.Column(db.VARCHAR(45), nullable=False)
+    LastName = db.Column(db.VARCHAR(45), nullable=False)
+    Email = db.Column(db.VARCHAR(45))
+    Password = db.Column(db.VARCHAR(256))
+    Phone = db.Column(db.VARCHAR(45))
+    UserStatus = db.Column(db.VARCHAR(45))
+
+    def get_roles(self):
+        return self.UserStatus
 
 
-# db.create_all()
+class Course(Base):
+    __tablename__ = 'Course'
 
-user = User(UserID=1, Username="rost", FirstName="Pantyo", LastName="Rostyslav", Email="@mail.com", Password='sadsdads', Phone="213231312", UserStatus="1")
-user1 = User(UserID=2, Username="rosst", FirstName="Passntyo", LastName="Rosatyslav", Email="@mdail.com", Password='sadfsdads', Phone="21323231312", UserStatus="1")
+    ID = db.Column(db.INTEGER)
+    Category = db.Column(db.VARCHAR(45))
+    Name = db.Column(db.VARCHAR(45), primary_key=True)
+    CourseHeadUsername = db.Column(db.VARCHAR(45), db.ForeignKey(User.Username))
+    CourseHead = db.relationship(User)
 
-course = Course(ID=2, Category="IT", Name="GoodCourse", AppliedStudent=user)
-# course1 = Course(ID=3, Category="ITS", Name="GoodCoursFSe", AppliedStudent=user1)
-course2 = Course(ID=4, Category="ITD", Name="GoodCourssfe", AppliedStudent=user)
-course = Course(ID=3, Category="IT", Name="GoodCourse", AppliedStudent=user1)
+    def get_course(self):
+        result = {
+            'id': self.ID,
+            'category': self.Category,
+            'name': self.Name,
+            'course_head_username': self.CourseHeadUsername
+        }
+        return result
 
-# UserCourse = UserCourses(ID=3, User=user1, Courses=course1)
-UserCourse1 = UserCourses(ID=4, User=user, Courses=course)
-db.session.add(user)
-# db.session.add(user1)
-db.session.add(course)
-# db.session.add(course1)
-db.session.add(course2)
-# db.session.add(UserCourse)
-db.session.add(UserCourse1)
 
-db.session.commit()
+class UserCourses(Base):
+    __tablename__ = 'UserCourses'
+
+    ID = db.Column(db.INTEGER, primary_key=True)
+    username = db.Column(db.VARCHAR(45), db.ForeignKey(User.Username))
+    User = db.relationship(User)
+    Courses_names = db.Column(db.VARCHAR(45), db.ForeignKey(Course.Name))
+    Courses = db.relationship(Course)
+    requestStatus = db.Column(db.VARCHAR(45))
+
+db.create_all()
